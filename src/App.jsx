c8193f81a41c33078240069e2c72f5be5a58c8e5
John Doe <johndoe@example.com>
2023-10-24 14:43:32 +0200

    LoginRout

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { Route, Routes } from "react-router"
+import { Route, Routes } from "react-router-dom"
 import SignUp from "./pages/SignUp"
+import Login from "./pages/Login"
 
 
 function App() {
 
   return (
     <>
-    <Routes>
-      <Route path='/signup' element={<SignUp/>} />
-    </Routes>
+      <Routes>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
-  )
+  );
 }
 
 export default App
