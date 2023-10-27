c69f468041d2b2b39473d991059719921d288f5a
John Doe <johndoe@example.com>
2023-10-27 15:04:43 +0200

    navbar

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
-
+import navbar from "../components/Navbar"
 const SignUp = () => {
   const navigate = useNavigate()
     const [username, setUsername] = useState('')
